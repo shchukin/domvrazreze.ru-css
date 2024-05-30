b25e24fcb5ad547cce0160b6e8b2b7f2d1870529
Anton Shchukin <a.a.shchukin@gmail.com>
2024-05-30 16:16:13 +0700

    playback

--- src/scripts/_collage.js
+++ src/scripts/_collage.js
@@ -1,57 +1,57 @@
-// (function ($) {
+(function ($) {
+
+    document.addEventListener("DOMContentLoaded", function() {
+
+        /* Playback */
+
+        const videos = document.querySelectorAll('.collage__video');
+
+        function playVideosSequentially(index) {
+            if (index < videos.length) {
+
+                /* Скрываем предыдущий */
+                if( videos[index - 1] ) {
+                    videos[index - 1].classList.remove('collage__video--currently-playing');
+                }
+
+                /* Показываем текущий */
+                videos[index].classList.add('collage__video--currently-playing');
+
+                /* Стартуем текущий */
+                videos[index].play();
+
+                /* рекурсивно запускаем следующий с задержкой в 1000ms */
+                setTimeout(function() {
+                    playVideosSequentially(index + 1);
+                }, videos[index].duration * 1000 + 1000);
+            }
+        }
+
+        videos[0].addEventListener('canplaythrough', function() {
+            playVideosSequentially(0);
+        });
+
+
+        /* Mute */
+
+        let isCollageMuted = true;
+        const unmute = document.querySelector('.collage__toggle-mute');
+        unmute.addEventListener('click', function() {
+            if (isCollageMuted) {
+                isCollageMuted = false;
+                this.classList.remove('collage__toggle-mute--muted');
+                videos.forEach(video => {
+                    video.muted = false;
+                })
+            } else  {
+                isCollageMuted = true;
+                this.classList.add('collage__toggle-mute--muted');
+                videos.forEach(video => {
+                    video.muted = true;
+                })
+            }
+        });
+    });
+
 //
-//         const videoCount = 4;
-//         const videosToLoad = 3;
-//         let videosLoaded = 0;
-//         let currentVideoIndex = 0;
-//         let loadedVideos = [];
-//
-//         // Function to start playing videos once the specified number are preloaded
-//         function startPlaying() {
-//             playNextVideo();
-//         }
-//
-//         // Function to play the next video
-//         function playNextVideo() {
-//             if (currentVideoIndex < videoCount) {
-//                 let $video = $(`#video-${currentVideoIndex + 1}`);
-//                 $video.addClass('collage__video--ready');
-//                 $video[0].play();
-//
-//                 $video.on('ended', function() {
-//                     $video.removeClass('collage__video--ready');
-//                     currentVideoIndex++;
-//                     setTimeout(playNextVideo, 1000); // Delay of 1 second
-//                 });
-//             }
-//         }
-//
-//         // Function to check if the specified number of videos are preloaded
-//         function checkVideosLoaded() {
-//             if (videosLoaded === videosToLoad) {
-//                 startPlaying();
-//             }
-//         }
-//
-//         // Add video elements to the page and set up preload handling
-//         for (let i = 1; i <= videoCount; i++) {
-//             let videoElement = `<video class="collage__video collage__video--${i}" id="video-${i}" src="../videos/collage-0${i}.mp4" preload="auto" muted></video>`;
-//             $('#video-container').append(videoElement);
-//
-//             let video = document.getElementById(`video-${i}`);
-//             video.addEventListener('canplaythrough', function() {
-//                 videosLoaded++;
-//                 loadedVideos.push(i);
-//                 checkVideosLoaded();
-//             });
-//         }
-//
-//
-//
-//
-//     // Function to unmute all videos
-//     $('.unmute').click(function() {
-//         $('video').prop('muted', false);
-//     });
-//
-// })(jQuery);
+})(jQuery);
