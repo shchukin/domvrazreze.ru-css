693f857ebb2d7c7c667807eaed693bbb32b31a06
Anton Shchukin <a.a.shchukin@gmail.com>
2024-05-31 14:13:52 +0700

    remainig videos, better quality, remove sound

--- src/scripts/_collage.js
+++ src/scripts/_collage.js
@@ -29,25 +29,5 @@ document.addEventListener("DOMContentLoaded", function() {
         playVideosSequentially(0);
     });
 
-
-    /* Mute */
-
-    let isCollageMuted = true;
-    const unmute = document.querySelector('.collage__toggle-mute');
-    unmute.addEventListener('click', function() {
-        if (isCollageMuted) {
-            isCollageMuted = false;
-            this.classList.remove('collage__toggle-mute--muted');
-            videos.forEach(video => {
-                video.muted = false;
-            })
-        } else  {
-            isCollageMuted = true;
-            this.classList.add('collage__toggle-mute--muted');
-            videos.forEach(video => {
-                video.muted = true;
-            })
-        }
-    });
 });
 
