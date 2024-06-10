38ded1863e8c5a864dc57e0bf275a6712c87e16e
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-10 23:13:29 +0700

    build

--- build/scripts/_header.js
+++ build/scripts/_header.js
@@ -3,6 +3,7 @@
     const $html = $('html');
 
     $('.header__player-toggle').on('click', function() {
+        $html.removeClass('menu-expanded');
         $html.toggleClass('playlist-expanded');
     });
 
@@ -11,16 +12,39 @@
     });
 
     $(document).on('click', function(event) {
-        if (!$(event.target).closest('.playlist__body, .header__player-toggle').length) {
+        if (!$(event.target).closest('.playlist__body, .header__player-toggle, .header__marquee').length) {
             $html.removeClass('playlist-expanded');
         }
     });
-
 
     $(document).on('keyup', function(event) {
         if (event.keyCode === 27) {
             $html.removeClass('playlist-expanded');
         }
     });
+
+
+
+    $('.header__burger').on('click', function() {
+        $html.removeClass('playlist-expanded');
+        $html.toggleClass('menu-expanded');
+    });
+
+    // $('.playlist__close').on('click', function() {
+    //     $html.removeClass('menu-expanded');
+    // });
+
+    // $(document).on('click', function(event) {
+    //     if (!$(event.target).closest('.playlist__body, .header__player-toggle').length) {
+    //         $html.removeClass('menu-expanded');
+    //     }
+    // });
+
+    $(document).on('keyup', function(event) {
+        if (event.keyCode === 27) {
+            $html.removeClass('menu-expanded');
+        }
+    });
+
 
 })(jQuery);
