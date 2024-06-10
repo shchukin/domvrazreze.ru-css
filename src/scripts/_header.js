b4a756eb3c85ef09c31c57cf8d51d1691b7ae74b
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-10 11:29:27 +0700

    playlist

--- src/scripts/_header.js
+++ src/scripts/_header.js
@@ -6,4 +6,21 @@
         $html.toggleClass('playlist-expanded');
     });
 
+    $('.playlist__close').on('click', function() {
+        $html.removeClass('playlist-expanded');
+    });
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.playlist__body, .header__player-toggle').length) {
+            $html.removeClass('playlist-expanded');
+        }
+    });
+
+
+    $(document).on('keyup', function(event) {
+        if (event.keyCode === 27) {
+            $html.removeClass('playlist-expanded');
+        }
+    });
+
 })(jQuery);
